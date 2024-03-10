0d39c47208d43f6a8accb8274a78086a7d4727a0
Dev Shakya <devxoshakya@gmail.com>
2024-03-10 19:51:54 +0530

    recursion-I

---
+++ src/fact.java
@@ -0,0 +1,18 @@
+public class fact {
+    public static void main(String[] args) {
+        System.out.println(sumofdig(1342));
+    }
+    static int Fact(int n){
+        if (n==0) {
+            return 1;
+        }
+        return n*Fact(n-1);
+    }
+
+    static int sumofdig(int n){
+        if(n==0){
+            return 0;
+        }
+        return n%10 + sumofdig(n/10);
+    }
+}
