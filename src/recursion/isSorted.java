d79c9aef69af2cc6605a651adac4e6a6224ca6af
Dev Shakya <devxoshakya@gmail.com>
2024-04-23 18:16:59 +0530

    added tex file

--- src/recursion/isSorted.java
+++ src/recursion/isSorted.java
@@ -1,9 +1,13 @@
 package recursion;
 
+import java.util.Scanner;
+
 public class isSorted {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,6,5};
         System.out.println(Sorted(arr, 0));
+        Scanner sc = new Scanner(System.in);
+        String n = sc.nextLine();
     }
     static boolean Sorted(int arr[], int index){
         if(index == arr.length-1){
